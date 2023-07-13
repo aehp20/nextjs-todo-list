98bb91735341f2f8dcd701b6626318f9609e0113
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2023-07-13 06:25:34 +0200

    Create a Form

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,47 +1,14 @@
 "use client"
 
-import Link from 'next/link';
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useRouter } from 'next/navigation';
+import Link from 'next/link'
 
-import { useTodoContext } from '../context/context';
-import { TodoContextType, TodoForm } from '../interfaces';
-
-type Inputs = {
-  title: string
-  description: string
-}
+import Form from '../components/Form'
 
 export default function AddPage () {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>()
-  const { save }= useTodoContext() as TodoContextType;
-
-  const onSubmit: SubmitHandler<Inputs> = (data: TodoForm) => {
-    console.log(data);
-    save(data);
-    router.push("/");
-  }
-
   return (
     <>
       <div>Add Todo</div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Title *</label>
-          <input defaultValue="" {...register("title", { required: true })} />
-          {errors.title && <span>This field is required</span>}
-        </div>
-        <div>
-          <label>Description</label>
-          <input defaultValue="" {...register("description")} />
-        </div>
-        <button type="submit">Add</button>
-      </form>
+      <Form />
       <div>
         <Link href="/">Go home</Link>
       </div>
--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,69 +1,36 @@
 "use client"
 
 import { useSearchParams } from 'next/navigation'
-import Link from 'next/link';
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useRouter } from 'next/navigation';
-import { useEffect } from "react"
+import Link from 'next/link'
+import { useEffect, useState } from "react"
 
-import { useTodoContext } from '../context/context';
-import { TodoContextType, TodoForm, Todo } from '../interfaces';
-
-type Inputs = {
-  title: string
-  description: string
-}
+import Form from '../components/Form'
+import { useTodoContext } from '../context/context'
+import { TodoContextType, Todo } from '../interfaces'
 
 export default function EditPage () {
-  const router = useRouter();
   const searchParams = useSearchParams()
-  const { todos } = useTodoContext() as TodoContextType;
-
-  const id = searchParams.get('id') as string
-
-  console.log('id', id);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset
-  } = useForm<Inputs>()
-  const { update }= useTodoContext() as TodoContextType;
-
-  const onSubmit: SubmitHandler<Inputs> = (data: TodoForm) => {
-    console.log(data);
-    const ediTodo: Todo = {id, ...data}
-    update(ediTodo)
-    router.push("/")
-  }
+  const { todos } = useTodoContext() as TodoContextType
+  const [todo, setTodo] = useState<Todo>()
 
   useEffect(() => {
+    const id = searchParams.get('id') as string
+
+    console.log('id', id)
+
     const foundTodo = todos.find(item=>item.id===id)
     if (foundTodo) {
-      reset({
-        title: foundTodo.title,
-        description: foundTodo.description
-      });
+      setTodo(foundTodo)
     }
-  }, [reset, id, todos]);
+  }, [])
 
-  return <div>
-    <div>Edit Todo</div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Title *</label>
-        <input defaultValue="" {...register("title", { required: true })} />
-        {errors.title && <span>This field is required</span>}
-      </div>
-      <div>
-        <label>Description</label>
-        <input defaultValue="" {...register("description")} />
-      </div>
-      <button type="submit">Edit</button>
-    </form>
+  return todo ? (
     <div>
-      <Link href="/">Go home</Link>
+      <div>Edit Todo</div>
+      <Form todo={todo} />
+      <div>
+        <Link href="/">Go home</Link>
+      </div>
     </div>
-  </div>
+  ) : null
 }
